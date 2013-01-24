ad2332ebf8b6c120d4af3852ea9541d2ce5db3c3
Matt Long <matt.long@matthew-long.com>
2013-01-24 10:31:01 -0700

    Initial Commit

---
+++ CollectionSearch/MLViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLViewController.h
+//  CollectionSearch
+//
+//  Created by Matt Long on 1/24/13.
+//  Copyright (c) 2013 Matt Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLViewController : UIViewController
+
+@end
---
+++ CollectionSearch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionSearch
+//
+//  Created by Matt Long on 1/24/13.
+//  Copyright (c) 2013 Matt Long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLAppDelegate class]));
+  }
+}
